38e33b6781a4cbe26c80726d42e86482e89e5bab
luckstar77 <luckstar77y@gmail.com>
2025-06-23 08:56:01 +0800

    Align navbar to left, login to right

--- src/layout/ButtonAppBar.tsx
+++ src/layout/ButtonAppBar.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import TemporaryDrawer from '../component/TemporaryDrawer';
 import { Link as RouterLink } from 'react-router-dom';
@@ -43,16 +42,10 @@ export default function ButtonAppBar() {
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    {/* Menu icon on small screens */}
+                    {/* Navigation on the left */}
                     {!isDesktop && <TemporaryDrawer />}
-
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        {/* <img src={StockLogo} style={{ width: '50px' }} /> */}
-                    </Typography>
-
-                    {/* Navigation links on desktop */}
                     {isDesktop && (
-                        <Box sx={{ mr: 2 }}>
+                        <Box sx={{ display: 'flex' }}>
                             <Button color="inherit" component={RouterLink} to="/">
                                 首頁
                             </Button>
@@ -76,6 +69,8 @@ export default function ButtonAppBar() {
                             </Button>
                         </Box>
                     )}
+
+                    <Box sx={{ flexGrow: 1 }} />
 
                     <Button color="inherit" style={{ fontSize: 18 }} onClick={handleClickOpen}>
                         {phone ? phone : '登入'}
