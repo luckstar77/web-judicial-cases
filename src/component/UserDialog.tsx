ab734f5e34c15bf1ac6c83624375786c1e49c670
luckstar77 <luckstar77y@gmail.com>
2025-06-27 16:51:30 +0800

    Show login success alert

--- src/component/UserDialog.tsx
+++ src/component/UserDialog.tsx
@@ -1,26 +1,25 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React from 'react';
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { PhoneAuthDialog } from './PhoneAuthDialog';
 import { UserDataDialog } from './UserDataDialog';
 import { USER_DIALOG_STATUS } from '../types/enums';
+import SuccessAlert from './SuccessAlert';
 
 
 export function UserDialog() {
 
-    const { showDialog } = useSelector(
-        (state: any) => state.user
-    );
+    const { showDialog } = useSelector((state: any) => state.user);
 
     return (
         <div>
-            {showDialog ===  USER_DIALOG_STATUS.PHONE_AUTH && (
+            {showDialog === USER_DIALOG_STATUS.PHONE_AUTH && (
                 <PhoneAuthDialog/>
             )}
-            {showDialog ===  USER_DIALOG_STATUS.USER_DATA && (
+            {showDialog === USER_DIALOG_STATUS.USER_DATA && (
                 <UserDataDialog/>
             )}
-
+            <SuccessAlert />
         </div>
     );
 }
